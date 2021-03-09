5555cd013df670159338f52bb9e8bbf7cec781ef
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-03-09 16:37:46 +0600

    1 Этап закончен .Админка,Минрация,фикстуры сделаны

--- issure_tracker/webapp/admin.py
+++ issure_tracker/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Tracer, Status, Type
 
+
+class TracersAdmin(admin.ModelAdmin):
+    list_display = ['id', 'surname', 'description', 'status', 'type']
+    list_filter = ['status', 'type']
+    fields = ['surname', 'description', 'status', 'type']
+
+
+admin.site.register(Tracer, TracersAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
 # Register your models here.
--- issure_tracker/webapp/models.py
+++ issure_tracker/webapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=100, verbose_name="Категории")
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Type(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=100, verbose_name="Тип")
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Tracer(models.Model):
+    surname = models.CharField(max_length=100, null=False, blank=False, verbose_name="Краткое описание")
+    description = models.TextField(max_length=2000, null=False, blank=False, verbose_name="Полное описание")
+    status = models.ForeignKey('webapp.Status', related_name='tracers', on_delete=models.PROTECT,
+                               verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', related_name='tracers', on_delete=models.PROTECT,
+                             verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    class Meta:
+        db_table = 'Tracers'
+        verbose_name = 'задача'
+        verbose_name_plural = 'задачи'
+
+    def __str__(self):
+        return f'{self.id}. {self.surname}:{self.description}{self.status} {self.type}'
