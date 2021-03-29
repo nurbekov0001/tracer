997becd6d791393f599dfbe1ab3a550284c047fb
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-03-29 16:17:18 +0600

    добавили новые формы для проекта

--- issure_tracker/webapp/forms.py
+++ issure_tracker/webapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from webapp.models import Tracer, Status, Type
+from webapp.models import Tracer, Project
 
 
 class TracerForm(forms.ModelForm):
@@ -14,3 +14,17 @@ class TracerDeleteForm(forms.Form):
 
 class SearchForm(forms.Form):
     search_value = forms.CharField(max_length=100, required=False, label='Найти')
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ('name', 'description', 'start_data', 'end_data')
+
+
+class ProjectDeleteForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label='Введите название задачи, чтобы удалить её')
+
+
+
+
