bb62572ae7b26521881a88c2818eb36f22aa2492
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-04-21 19:10:50 +0600

    добавил пути изменил некоторые моменты

--- issure_tracker/accounts/urls.py
+++ issure_tracker/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from accounts.views import logout_view, login_view, register_view, UserDetailView, UserView
+from accounts.views import logout_view, login_view, register_view, UserDetailView, UserView,\
+    UserChangeView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('create/', register_view, name='create'),
     path('<int:pk>/', UserDetailView.as_view(), name='detail'),
-    path('users/', UserView.as_view(), name='user')
+    path('users/', UserView.as_view(), name='user'),
+    path('<int:pk>/change/', UserChangeView.as_view(), name='change'),
+    path('password_change/', UserPasswordChangeView.as_view(), name='password_change')
 
 ]
